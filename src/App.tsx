85ed35605d42782f9e56b7c9cb79d18dc89355df
Ismael Torres González <ismaeltorresgon@gmail.com>
2025-02-21 13:02:40 +0100

    update App component to feature EscolaVision branding and logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,13 @@ function App() {
   return (
     <>
       <div className="flex justify-center items-center space-x-4 p-4">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo w-16 h-16" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react w-16 h-16" alt="React logo" />
-        </a>
+        <img src="/src/assets/escolavision.png" alt="EscolaVision Logo" className="h-64" />
       </div>
-      <h1 className="text-3xl font-bold underline text-center">Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-center">EscolaVision</h1>
+      <h3 className="text-2xl text-center">Tu App de Orientación Escolar</h3>
       <div className="card flex flex-col items-center p-4">
-        <button
+        
+        {/* <button
           className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
           onClick={() => setCount((count) => count + 1)}
         >
@@ -27,10 +24,10 @@ function App() {
         <p className="mt-4">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs text-center mt-4">
+        <p className="read-the-docs text-center mt-4">
         Click on the Vite and React logos to learn more
-      </p>
+      </p>*/}
+      </div>
     </>
   )
 }
